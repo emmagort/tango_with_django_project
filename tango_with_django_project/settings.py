1aed4c22ca1fe1a82ad4a771f8db4656b2653d4b
Emma Gort <2577478G@student.gla.ac.uk>
2022-01-31 21:58:12 +0000

    Chapter 4.2"

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-print(TEMPLATE_DIR)
+STATIC_DIR= os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rango',
 ]
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
